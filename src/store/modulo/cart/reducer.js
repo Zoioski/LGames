4a2e9dd3065a69081ea538ec5faca23fa2daa9d7
Zoioski <gustavoae@live.com>
2020-08-16 13:50:22 -0300

    Hi

--- src/store/modulo/cart/reducer.js
+++
@@ -1,9 +0,0 @@
-export default function cart(state = [], action) {
-  console.log(state)
-  switch (action.type) {
-    case 'ADD_TO_CART':
-      return [action.products]
-    default:
-      return state
-  }
-}
